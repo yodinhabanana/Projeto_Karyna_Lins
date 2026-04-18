d5bc27413402c573d5e6eb686cdaa0d4ba9393ae
Karyna Morais Lins <karynamolins@gmail.com>
2026-04-18 14:46:43 -0300

    Criação da classe C

---
+++ C.java
@@ -0,0 +1,36 @@
+public class C {
+
+    private String C1;
+    private int C2;
+
+    public C() {
+
+        C1 = "";
+        C2 = 0;
+
+    }
+    
+    public String getC1() {
+
+        return C1;
+
+    }
+
+    public int getC2() {
+
+        return C2;
+
+    }
+
+    public void MC1() {
+
+        System.out.println("MC1");
+        
+    }
+
+    public void MC2() {
+
+        System.out.println("MC2");
+
+    }
+}
