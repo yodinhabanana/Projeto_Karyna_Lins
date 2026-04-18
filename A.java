b8f07f26333a5b0202dac760541efe46ac461c3d
Karyna Morais Lins <karynamolins@gmail.com>
2026-04-18 14:19:15 -0300

    Criação de nova funcionalidade com classes A e B em java bem legalzinhas

---
+++ A.java
@@ -0,0 +1,37 @@
+public class A {
+
+    private int A1;
+    private float A2;
+
+    public A() {
+
+        A1 = 0;
+        A2 = 0;
+
+    }
+
+    public int getA1() {
+
+        return A1;
+
+    }
+
+    public float A2() {
+
+        return A2;
+
+    }
+
+    public void MA1() {
+
+        System.out.println("MA1");
+
+
+    }
+
+    public void MA2() {
+
+        System.out.println("MA2");
+
+    }
+}
---
+++ B.java
@@ -0,0 +1,37 @@
+public class B {
+
+    private int B1;
+    private float B2;
+
+    public B() {
+
+        B1 = 0;
+        B2 = 0;
+
+    }
+
+    public int getB1() {
+
+        return B1;
+
+    }
+
+    public float B2() {
+
+        return B2;
+
+    }
+
+    public void MB1() {
+
+        System.out.println("MB1");
+
+
+    }
+
+    public void MB2() {
+
+        System.out.println("MB2");
+
+    }
+}
