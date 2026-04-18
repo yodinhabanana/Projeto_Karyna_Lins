5ef1ab96ecc1c8694823e08fe7a63292366f39e8
Karyna Morais Lins <karynamolins@gmail.com>
2026-04-18 15:04:26 -0300

    Nova classe D adicionada

---
+++ D.java
@@ -0,0 +1,37 @@
+public class D {
+
+    private int D1;
+    private float D2;
+
+    public D() {
+
+        D1 = 0;
+        D2 = 0;
+
+    }
+
+    public int getD1() {
+
+        return D1;
+
+    }
+
+    public float D2() {
+
+        return D2;
+
+    }
+
+    public void MD1() {
+
+        System.out.println("MD1");
+
+
+    }
+
+    public void MD2() {
+
+        System.out.println("MD2");
+
+    }
+}
